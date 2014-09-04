a03f9e12868564e3c17eae7598b03a95eb71fd14
MaximShirokov <maximshirokov21@gmail.com>
2014-09-04 17:10:00 +0400

    Create photo model and something for messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def app_online_path online
+    online_path(username: online.user.username, id: online.id)
+  end
 end
